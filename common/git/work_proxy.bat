fa7c6b79d53c1e944f6df16e6fb23b2cc7116e3b
andersqiu <qiuzhong0917@gmail.com>
2017-11-07 09:28:41 +0800

    Add git proxy setting scripts for work

---
+++ common/git/work_proxy.bat
@@ -0,0 +1,4 @@
+:: Git proxy for work
+
+git config --global http.proxy http://child-prc.intel.com:913
+git config --global https.proxy http://child-prc.intel.com:913
---
+++ common/git/work_proxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Git proxy for work
+git config --global http.proxy http://child-prc.intel.com:913
+git config --global https.proxy http://child-prc.intel.com:913
