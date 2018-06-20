a8ebf67e2f0dfbb70932c828270dfa7ba6ca1de8
andersqiu <qiuzhong0917@gmail.com>
2018-06-20 18:03:36 +0800

    Correct sqlite3 dependency package name

--- linux/ubuntu/install_py3_dep_pkgs.sh
+++ linux/ubuntu/install_py3_dep_pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install -y libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev libsqlite3-devel libssl-dev tk-dev libreadline-dev
+sudo apt install -y libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev libsqlite3-dev libssl-dev tk-dev libreadline-dev
