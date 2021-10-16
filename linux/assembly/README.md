728e7cdf39cf8ce0fead93df7244d3f774890326
andersqiu <qiuzhong0917@gmail.com>
2021-10-16 22:27:26 +0800

    Add the hello world example for Linux x86 platform

---
+++ linux/assembly/README.md
@@ -0,0 +1,6 @@
+# Introduction
+x86 assembly language example on Linux
+
+## Examples
+* Hello World example for both Linux x86 and x86-64
+
---
+++ linux/assembly/x86/hello.s
@@ -0,0 +1,17 @@
+.section .data
+hello_world:
+	.ascii "Hello World!\n"
+
+.section .text
+.global _start
+_start:
+	movl $4, %eax
+	movl $1, %ebx
+	movl $hello_world, %ecx
+	movl $14, %edx
+	int $0x80
+
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
+
