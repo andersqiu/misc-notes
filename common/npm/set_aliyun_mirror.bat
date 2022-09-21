b5959b84325ff0befb904d0501e7528b5e1fe29c
andersqiu <qiuzhong0917@gmail.com>
2022-09-21 08:39:22 +0800

    Add npm aliyun mirror script
    Add two scripts to use npm aliyun mirror for acceleration
    while installing npm modules.

---
+++ common/npm/set_aliyun_mirror.bat
@@ -0,0 +1,6 @@
+:: Use npm Aliyun mirror for acceleration
+npm config set registry https://registry.npmmirror.com
+
+:: In case there is the "certificate has expired" error
+npm config set strict-ssl false
+
---
+++ common/npm/set_aliyun_mirror.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use npm Aliyun mirror for acceleration
+npm config set registry https://registry.npmmirror.com
+
+# In case there is the "certificate has expired" error
+npm config set strict-ssl false
+
